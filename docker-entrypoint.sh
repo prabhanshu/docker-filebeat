172540e7939e86b14b49e6cb4074d7822725a2dd
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2016-03-21 17:33:46 +0530

    added filebeat logs path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Render config file
-cat filebeat.yml | sed "s/LOGSTASH_HOST/$LOGSTASH_HOST/" | sed "s/LOGSTASH_PORT/$LOGSTASH_PORT/" | sed "s/INDEX/$INDEX/" > filebeat.yml.tmp
+cat filebeat.yml | sed "s/LOGSTASH_HOST/$LOGSTASH_HOST/" | sed "s/LOGSTASH_PORT/$LOGSTASH_PORT/" | sed "s/LOGSTASH_INDEX/$LOGSTASH_INDEX/"  | sed "s/LOGSTASH_PATH/$LOGSTASH_PATH/"> filebeat.yml.tmp
 cat filebeat.yml.tmp > filebeat.yml
 rm filebeat.yml.tmp
 
